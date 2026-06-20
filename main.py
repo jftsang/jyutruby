1f168b1b0e17172b7a002ed66f4e5fef1049df0f
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2026-06-20 13:45:39 +0100

    add pinyin support, not on frontend yet

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 from streamerate import stream
 
+from pypinyin import pinyin, Style
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -40,14 +41,45 @@ def character_to_jyutping(character: str) -> tuple[str, str]:
     return character, jyut
 
 
+def character_to_pinyin(character: str) -> tuple[str, str]:
+    """Wrapper around pypinyin that processes a single character,
+    and is guaranteed to return a value even for non-Chinese characters
+    as well as special characters like \n.
+    """
+
+    # Avoid silly edge cases like the English character `e` or Arabic
+    # numerals like `2` being interpreted and pinyinised.
+    if character in string.printable:
+        return (character, "")
+
+
+    charpin = pinyin(character, style=Style.TONE, heteronym=False)
+    if not charpin or not charpin[0]:  # empty list, for \n and other special characters
+        return (character, "")
+
+    pin = charpin[0][0]
+    if pin is None:  # e.g. for punctuation and Latin characters
+        pin = ""
+
+    return character, pin
+
+
 def chinese_to_jyutping(chinese: str) -> list[tuple[str, str | None]]:
-    """Returns list of possible jyutping readings for a single character."""
     return stream(chinese).map(character_to_jyutping).to_list()
 
+def chinese_to_pinyin(chinese: str) -> list[tuple[str, str | None]]:
+    """Returns list of possible pinyin readings for a single character."""
+    return stream(chinese).map(character_to_pinyin).to_list()
 
-@app.get("/transcribe")
-async def transcribe(chinese: str) -> list[tuple[str, str | None]]:
+
+@app.get("/jyutping")
+async def jyutping_api(chinese: str) -> list[tuple[str, str | None]]:
     return chinese_to_jyutping(chinese)
+
+
+@app.get("/pinyin")
+async def pinyin_api(chinese: str) -> list[tuple[str, str | None]]:
+    return chinese_to_pinyin(chinese)
 
 
 @app.get("/")
