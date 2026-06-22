3672f9dea93b9bf92c7ae737b728d5f0d90f57bd
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2026-06-22 01:25:11 +0100

    preserve lines option

--- src/jyutruby.js
+++ src/jyutruby.js
@@ -3,14 +3,16 @@ import {defaultText} from "./defaulttext.js";
 import {cantojpmin_data} from "./CantoJpMin/scripts/modules_format/cantojpmin_data.js";
 
 const initialState = {
-  inputText: defaultText, annotatedCharacters: new Set(), showingAll: false,
+  inputText: defaultText,
+  annotatedCharacters: new Set(),
+  showingAll: false,
+  preservingLines: true,
 };
 
 function toJyut(char) {
   if (Object.hasOwn(cantojpmin_data, char)) return CantoJpMin.toJyutping(char);
   return null;
 }
-
 
 const actions = {
   setInputText: (state, event) => {
@@ -25,15 +27,19 @@ const actions = {
     return {...state, annotatedCharacters: annotatedCharacters};
   }, toggleShowAll: (state) => {
     return {...state, showingAll: !state.showingAll};
+  }, togglePreservingLines: (state) => {
+    return {...state, preservingLines: !state.preservingLines};
   }, hideAll: (state) => {
     return {...state, annotatedCharacters: new Set(), showingAll: false};
   },
 };
 
-const displayCharacter = (char, isVisible) => {
+const displayCharacter = (char, state) => {
   if (char === '\n') {
+    if (!state.preservingLines) return;
     return h('br', {class: ''}, []);
   }
+  const isVisible = state.annotatedCharacters.has(char) || state.showingAll
   const jyut = toJyut(char);
   if (!jyut) {
     return text(char);
@@ -46,15 +52,26 @@ const displayCharacter = (char, isVisible) => {
 
 function view(state) {
   const showAllLabel = h('label', {'for': 'showAllToggle'}, [text('Show all')]);
-  const showAll = h('input', {id: 'showAllToggle', type: 'checkbox', onclick: actions.toggleShowAll}, [text(showAllLabel)]);
+  const showAll = h('input', {
+    id: 'showAllToggle',
+    type: 'checkbox',
+    checked: state.showingAll,
+    onclick: actions.toggleShowAll
+  }, [text(showAllLabel)]);
+
+  const preserveLinesLabel = h('label', {'for': 'preserveLinesToggle'}, [text('Preserve lines')]);
+  const preserveLines = h('input', {
+    id: 'preserveLinesToggle',
+    type: 'checkbox',
+    checked: state.preservingLines,
+    onclick: actions.togglePreservingLines
+  }, []);
   const hideAll = h('button', {onclick: actions.hideAll}, [text('Hide all')]);
   const topBar = h('div', {
     class: 'topbar',
-  }, [showAll, showAllLabel, hideAll])
+  }, [showAll, showAllLabel, hideAll, preserveLines, preserveLinesLabel])
 
-  const annotatedDisplay = h('div', {id: 'annotated'},
-    Array(...state.inputText).map(char => displayCharacter(char, state.annotatedCharacters.has(char) || state.showingAll))
-  );
+  const annotatedDisplay = h('div', {id: 'annotated'}, Array(...state.inputText).map(char => displayCharacter(char, state)));
   const inputForm = h('div', {style: {padding: "50px", height: "40%"}}, [h('textarea', {
     id: 'chinesetext',
     name: 'chinesetext',
