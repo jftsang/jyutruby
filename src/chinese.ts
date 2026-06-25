b83bd39a725c3970d91e66a0453a259df885ec80
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2026-06-25 22:34:56 +0100

    eliminate some ts-ignores

--- src/chinese.ts
+++ src/chinese.ts
@@ -1,8 +1,8 @@
-import {cantojpmin_data} from "./CantoJpMin/scripts/modules_format/cantojpmin_data.js";
+// jyutping lookup
+const {CantoJpMin, cantojpmin_data} = (window as any);
 
 export function toJyutping(char: string): string | null {
     if (Object.hasOwn(cantojpmin_data, char)) {
-        // @ts-ignore
         return CantoJpMin.toJyutping(char);
     } else {
         return null;
@@ -10,8 +10,7 @@ export function toJyutping(char: string): string | null {
 }
 
 export function toJyutpingArray(char: string): string[] | null {
-    if (Object.hasOwn(cantojpmin_data, char)) {
-        // @ts-ignore
+  if (Object.hasOwn(cantojpmin_data, char)) {
         return CantoJpMin.toJyutpingArray(char)[0].jyutpings;
     } else {
         return null;
